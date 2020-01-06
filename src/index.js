4400de3b3566b46d7d8ab17a4b93fb7200648791
Aminessid <essidmohamedamin@gmail.com>
2020-01-06 19:10:59 +0100

    now it works in IE

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
+import 'react-app-polyfill/ie9';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { BrowserRouter as Router } from "react-router-dom";
 import { RoomProvider } from "./Context";
-import 'react-app-polyfill/ie9';
+
 
 ReactDOM.render(
     <RoomProvider>
