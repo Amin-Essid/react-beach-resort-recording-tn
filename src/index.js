08a69c851d0017884c10b6879fe84c8b5591a73d
Aminessid <essidmohamedamin@gmail.com>
2020-01-06 18:39:06 +0100

    with IE support

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { BrowserRouter as Router } from "react-router-dom";
 import { RoomProvider } from "./Context";
+import 'react-app-polyfill/ie9';
 
 ReactDOM.render(
     <RoomProvider>
