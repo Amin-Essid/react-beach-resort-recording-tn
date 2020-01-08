ff4c455eb07ca0b025b924323aebde2eccba9ad3
Aminessid <essidmohamedamin@gmail.com>
2020-01-08 16:19:28 +0100

    please

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -13,13 +13,13 @@ export default class Navbar extends Component {
         this.setState({isOpen:!this.state.isOpen})
     }
     render() {
-        return (
+        return ( 
             <nav className="navbar">
                 <div className="nav-center">
                     <div className="nav-header">
                         <Link to="/">
                         <img src={logo} alt="Beach Resort"/></Link>
-                        <button type="button" className="nav-btn" onClick={this.hundelToggle}>
+                        <button type="button" className="nav-btn" onClick={this.handleToggle}>
                             <FaAlignRight className="nav-icon" />
                         </button>
                     </div>
